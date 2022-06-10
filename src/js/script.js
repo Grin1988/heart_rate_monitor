83060d2105f501b1fc50d11e7e0a994c585f03ff
Evgeniy <sogrin_es@mail.ru>
2022-06-11 00:10:15 +0300

    js

---
+++ src/js/script.js
@@ -0,0 +1,51 @@
+/* var name = "Evgeniy";
+
+const pi = 3.14;
+let number = 7;
+
+number = 4;
+pi = 5;
+
+let leftBorderWidth = 200; */
+
+// number
+// string - "", '', ``
+// true/false
+// null
+// underfined
+
+/* let obj = {
+    name: 'apple',
+    color: 'green',
+    weight: 200
+} */
+
+/* symbol */
+
+/* alert(1234); */
+
+
+/* let answer = confirm("Вам есть 18?");
+console.log(answer); */
+
+/* let answer = prompt("Вам есть 18?", "");
+console.log(answer); */
+
+/* console.log(4 + 'fdd') */
+
+/* let isChecked = true,
+    isClose = true; */
+
+/* console.log(isChecked && isClose); */
+
+/* console.log(isChecked || isClose); */
+
+/* if (2 * 4 == 8 * 1) {
+    console.log('Верно')
+} else {
+    console.log('Ошибка')
+} */
+
+for (let i = 1; i < 8; i++) {
+    console.log(i)
+}
