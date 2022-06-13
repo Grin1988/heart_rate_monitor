673ab5a024a6bab5fab43aa9b7eb1b66ae8636d9
Evgeniy <sogrin_es@mail.ru>
2022-06-13 23:14:59 +0300

    js

--- src/js/script.js
+++ src/js/script.js
@@ -1,51 +1,15 @@
-/* var name = "Evgeniy";
-
-const pi = 3.14;
-let number = 7;
-
-number = 4;
-pi = 5;
-
-let leftBorderWidth = 200; */
-
-// number
-// string - "", '', ``
-// true/false
-// null
-// underfined
-
-/* let obj = {
-    name: 'apple',
-    color: 'green',
-    weight: 200
-} */
-
-/* symbol */
-
-/* alert(1234); */
-
-
-/* let answer = confirm("Вам есть 18?");
-console.log(answer); */
-
-/* let answer = prompt("Вам есть 18?", "");
-console.log(answer); */
-
-/* console.log(4 + 'fdd') */
-
-/* let isChecked = true,
-    isClose = true; */
-
-/* console.log(isChecked && isClose); */
-
-/* console.log(isChecked || isClose); */
-
-/* if (2 * 4 == 8 * 1) {
-    console.log('Верно')
-} else {
-    console.log('Ошибка')
-} */
-
-for (let i = 1; i < 8; i++) {
-    console.log(i)
-}
+$(document).ready(function() {
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-solid.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right-solid.png" alt=""></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }]
+    });
+});
